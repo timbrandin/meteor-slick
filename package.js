8793d36fa6a19d752f9b71456bf230639e2189ad
Tim Brandin <tim.brandin@gmail.com>
2015-07-26 23:54:50 +0200

    New version of Slick 1.5.8

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-  summary: "the last carousel you'll ever need",
+  summary: "The last carousel you'll ever need.",
   name:    "dcsan:slick",
-  version: "1.4.1_2",
+  version: "1.5.8_1",
   git:     "https://github.com/dcsan/meteor-slick"
 });
 
 Package.onUse(function(api) {
-  
+
   api.versionsFrom("METEOR@1.0");
-  
+
   api.use('jquery', 'client');
-  
+
   api.addFiles([
     "vendor/slick/slick/ajax-loader.gif",
     "vendor/slick/slick/fonts/slick.eot",
@@ -18,12 +18,11 @@ Package.onUse(function(api) {
     "vendor/slick/slick/fonts/slick.ttf",
     "vendor/slick/slick/fonts/slick.woff"
   ], "client", { isAsset: true });
-  
+
   api.addFiles([
     "vendor/slick/slick/slick.css",
     "vendor/slick/slick/slick.js",
     "lib/overrides.css"
   ], "client");
-  
-});
 
+});
